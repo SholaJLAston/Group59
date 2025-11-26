90bc8e1c34ba2406e73fda9bcc49f3c01044a623
DukeWollemi <omarelshora396@gmail.com>
2025-11-26 09:39:01 +0000

    Laravel Migrations for products, and baskets.

---
+++ database/migrations/create_baskets_table.php
@@ -0,0 +1,22 @@
+<?php
+
+// WARNING: Do NOT run this migration on the server yet. Still subject to tests.
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+  public function up() : void {
+      Schema::create('baskets', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->timestamp();
+      });
+  }
+
+  public function down() : void {
+      Schema::dropIfExists('baskets');
+  }
+};
---
+++ database/migrations/create_products_table.php
@@ -0,0 +1,25 @@
+<?php
+
+// WARNING: Do NOT run this migration on the server yet. Still subject to tests.
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->index();
+            $table->text('description');
+            $table->decimal('price', 8, 2)->index();
+            $table->unsignedInteger('stock_quantity');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void {
+        Schema::dropIfExists('products');
+    }
+};
