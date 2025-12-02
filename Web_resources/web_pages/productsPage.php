c291493e87b3e283a030e25cf3cce328a66ca469
adama <240196608@aston.ac.uk>
2025-12-02 08:29:19 +0000

    file changes - signup page, login page, initial products page setup

---
+++ Web_resources/web_pages/productsPage.php
@@ -0,0 +1,18 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Short description of page">
+
+        <!-- Resources -->
+        <link rel="stylesheet" href="../StyleSheets/mainColours.css">
+    </head>
+    <body>
+        <h1>products page</h1>
+    </body>
+</html>
