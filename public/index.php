1309bad831578f42da03587528345caf879a898a
adama <240196608@aston.ac.uk>
2025-12-02 13:27:52 +0000

    new file format - index page, signup page, login page and resources in correct folders

--- public/index.php
+++ public/index.php
@@ -1,20 +1,21 @@
 <?php
+    include("../resources/views/header.html");
+?>
 
-use Illuminate\Foundation\Application;
-use Illuminate\Http\Request;
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Short description of page">
 
-define('LARAVEL_START', microtime(true));
+        <title>Index</title>
 
-// Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
-
-// Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
-
-// Bootstrap Laravel and handle the request...
-/** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-$app->handleRequest(Request::capture());
+        <!-- Resources -->
+        <link rel="stylesheet" href="../resources/css/Main.css">
+         
+    </head>
+    <body>
+        <h1>Index Page</h1>
+    </body>
+</html>
--- resources/views/login.php
+++ resources/views/login.php
@@ -1,3 +1,7 @@
+<?php
+    include("header.html");
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,21 +10,9 @@
     <title>Account Log In - Apex Hardware</title>
 
     
-    <link rel="stylesheet" href="../StyleSheets/index.css">
+    <link rel="stylesheet" href="../css/Main.css">
 </head>
 <body>
-    <div id="backgroundLogo">
-        <img src="../images/main_logo.png" width="299px" height="251px" style="opacity: 0.85;">
-    </div>
-
-    <div class="flexLinks">
-        <a href="aboutus.html" class="flexLinks"><p>About Us</p></a>
-        <a href="contactus.html" class="flexLinks"><p>Contact Us</p></a>
-        <a href="shopnow.html" class="flexLinks"><p>Shop Now</p></a>
-        <a href="signup.html" class="flexLinks"><p>Sign Up</p></a>
-        <a href="login.html" class="flexLinks"><p>Account Log In</p></a>
-    </div>
-
     <div class="auth-wrapper">
         <div class="auth-card">
             <h1 class="auth-title">Account Log In</h1>
@@ -41,7 +33,7 @@
 
                 <p class="auth-switch">
                     Don’t have an account yet?
-                    <a href="signup.html">Create one</a>
+                    <a href="signup.php">Create one</a>
                 </p>
             </form>
         </div>
