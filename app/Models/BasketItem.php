2dd2d40eab19dfde88154322783dfd060a53e27e
root <root@OmarElshora.localdomain>
2025-12-04 17:20:05 +0000

    Basket models

--- app/Models/BasketItem.php
+++ app/Models/BasketItem.php
@@ -4,15 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+class BasketItem extends Model {
     protected $fillable = [
         'basket_id',
         'product_id',
         'quantity',
     ];
 
+    public function basket() {
         return $this->belongsTo(Basket::class);
     }
 
+    public function product() {
         return $this->belongsTo(Product::class);
     }
 }
