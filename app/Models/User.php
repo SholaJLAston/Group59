52df0cd590651c03c98187a15aed52dc8cea47ca
b-ashir <184867125+b-ashir@users.noreply.github.com>
2025-12-04 01:13:07 +0000

    Implement core models and relationships

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,8 +4,11 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+
 
 class User extends Authenticatable
 {
@@ -18,9 +21,13 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
         'email',
         'password',
+        'password_changed',
+        'phone_number',
+        'role',
     ];
 
     /**
@@ -43,6 +50,24 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'password_changed' => 'boolean'
         ];
+    }
+
+
+
+    public function basket(): HasOne
+    {
+        return $this->hasOne(Basket::class);
+    }
+
+        public function orders(): HasMany
+    {
+        return $this->hasMany(Order::class);
+    }
+
+        public function contactRequest(): HasMany
+    {
+        return $this->hasMany(ContactRequest::class);
     }
 }
