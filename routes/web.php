75bbf22fbee875191a775d49ed9aab903342464d
b-ashir <184867125+b-ashir@users.noreply.github.com>
2025-12-05 05:55:20 +0000

    Adds routes for products page

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,8 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/about', [AboutController::class, 'index'])->name('about');
 Route::get('/contact', [ContactController::class, 'create'])->name('contact.create');
 Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
-
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
