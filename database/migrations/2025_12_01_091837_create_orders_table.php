c88cb3acbd094ac13bc14e9073ab612713658f64
root <root@OmarElshora.localdomain>
2026-03-24 20:50:00 +0000

    DB fix.

--- database/migrations/2025_12_01_091837_create_orders_table.php
+++ database/migrations/2025_12_01_091837_create_orders_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->string('order_number')->unique();
             $table->decimal('price', 8, 2);
             $table->enum('status', ['pending', 'processing', 'shipped', 'delivered', 'cancelled'])
                 ->default('pending');
